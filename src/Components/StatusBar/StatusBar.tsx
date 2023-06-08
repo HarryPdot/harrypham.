bca75ab80e4ecd887c59a7dc750f5cab6575a705
Harry <harrypham_@hotmail.com>
2023-06-08 22:11:26 +1000

    create icons in statusbar

--- src/Components/StatusBar/StatusBar.tsx
+++ src/Components/StatusBar/StatusBar.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { Container } from "../Resuables/Container/Container";
 import profilePic from "../../Assets/Images/tempharry.png";
+import { GoMarkGithub as GitHub } from "react-icons/go";
+import { SiLinkedin as LinkedIn } from "react-icons/si";
+import { BsInstagram as Instagram } from "react-icons/bs";
 
 const StatusBar = () => {
   return (
@@ -14,6 +17,7 @@ const StatusBar = () => {
           items-center
           justify-center
           text-light
+          gap-x-4
           "
     >
       <section
@@ -27,7 +31,23 @@ const StatusBar = () => {
         <Image src={profilePic} width={200} height={200} alt="profile image" />
         <h3>Software Engineer</h3>
       </section>
-      <section>icon</section>
+      <section
+        className="
+      flex
+      flex-col
+      gap-5
+      "
+      >
+        <a href="https://google.com" target="_blank">
+          <GitHub className="text-3xl hover:text-blue-50"></GitHub>
+        </a>
+        <a href="https://google.com" target="_blank">
+          <LinkedIn className="text-3xl hover:text-blue-50"></LinkedIn>
+        </a>
+        <a href="https://google.com" target="_blank">
+          <Instagram className="text-3xl hover:text-blue-50"></Instagram>
+        </a>
+      </section>
     </Container>
   );
 };
