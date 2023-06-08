f1c930009e4e0da5c4374b950b4d965e18aadf45
Harry <harrypham_@hotmail.com>
2023-06-08 17:17:28 +1000

    create cursor effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+"use client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return <div>hello</div>;
+  const [xVar, setXVar] = useState<Number>(0);
+  const [yVar, setYVar] = useState<Number>(0);
+  const handleCursor = (e: any) => {
+    setXVar(e.clientX);
+    setYVar(e.clientY);
+  };
+
+  useEffect(() => {
+    const pos = document.documentElement;
+    pos.style.setProperty("--x", xVar + "px");
+    pos.style.setProperty("--y", yVar + "px");
+  }, [xVar, yVar]);
+  return (
+    <div
+      className="
+      testing
+      pointer-events-auto 
+      m-0
+      h-full
+      w-full
+      bg-darkMode
+      text-light
+    "
+      onMouseMove={handleCursor}
+    >
+      hello
+    </div>
+  );
 }
