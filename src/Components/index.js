e7294a5f68fce718838fe654547d873f1574a20f
Harry <harrypham_@hotmail.com>
2023-06-22 12:33:08 +1000

    create pages | install contentful

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
 import { Container } from "./Resuables/Container/Container";
 import { StatusBar } from "./StatusBar/StatusBar";
 import { ChatBubble } from "./ChatBubble/ChatBubble";
+import { About } from "./Pages/About/About";
 
-export { Container, StatusBar, ChatBubble };
+export { Container, StatusBar, ChatBubble, About };
