197efdc41a3c830029aa4470fd188d8f49a9e51e
Harry <harrypham_@hotmail.com>
2023-06-29 19:24:59 +1000

    edit projects

--- src/Components/Pages/Projects/Projects.tsx
+++ src/Components/Pages/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import "./Projects.css";
 const Projects = ({ setOpen, setSelectProject }: any) => {
   const { getProjectContent } = useContentful();
   const [projects, setProjects] = useState<any>([]);
+  const [hovered, setHovered] = useState<String>("");
 
   useEffect(() => {
     getProjectContent().then((res) => {
@@ -27,29 +28,44 @@ const Projects = ({ setOpen, setSelectProject }: any) => {
     setSelectProject(item);
   };
 
+  const handleHover = (item: String) => {
+    setHovered(item);
+  };
+
   return (
-    <section className="flex flex-wrap gap-3 justify-center overflow-y-scroll w-full h-full absolute pt-2 pb-2 scrolling">
-      {projects.map((item: any, i: number) => {
-        return (
-          <div
-            key={i}
-            className="border border-white w-1/4 h-52 cursor-pointer relative rounded-md overflow-hidden hover:text-white"
-            onClick={() => handleOpen(item)}
-          >
-            <picture className="">
-              <img
-                src={item.image}
-                alt=""
-                className="w-full h-full object-cover absolute "
-              />
-            </picture>
-            <div className="w-full h-full bg-blend-overlay backdrop-blur-sm absolute bg-black opacity-60 rounded-lg"></div>
-            <div className="absolute w-full h-full flex justify-center items-center p-5 text-center">
-              <h2 className="text-lg">{item.name}</h2>
+    <section className="flex justify-evenly items-center flex-col h-full absolute">
+      <section className="flex flex-wrap gap-3 justify-center items-end overflow-y-scroll w-full h-fit  pt-2 pb-2 scrolling">
+        {projects.map((item: any, i: number) => {
+          return (
+            <div
+              key={i}
+              className="border border-white w-52 h-32 cursor-pointer relative rounded-md overflow-hidden hover:text-white"
+              onClick={() => {
+                handleOpen(item);
+              }}
+              onMouseOver={() => handleHover(item.name)}
+            >
+              <picture className="">
+                <img
+                  src={item.image}
+                  alt=""
+                  className="w-full h-full object-cover absolute "
+                />
+              </picture>
+              <div className="w-full h-full bg-blend-overlay backdrop-blur-sm absolute bg-black opacity-60 rounded-lg"></div>
+              <div className="absolute w-full h-full flex justify-center items-center p-5 text-center">
+                <h2 className="text-lg">{item.name}</h2>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </section>
+      {hovered ? (
+        <section className="w-full h-16 flex justify-center items-center relative">
+          <div className="w-full bg-black opacity-50 absolute h-full"></div>
+          <div className="text-xl absolute">{hovered}</div>
+        </section>
+      ) : null}
     </section>
   );
 };
